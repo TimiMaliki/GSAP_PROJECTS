4a3614a2ed65046080bfa6d373928816a085013a
TimiMaliki <malikitimi@gmail.com>
2025-05-24 23:01:48 +0100

    added : scrollTrigger-toggleActions

--- AlabiElite/src/js/index.js
+++ AlabiElite/src/js/index.js
@@ -58,12 +58,53 @@ document.addEventListener("DOMContentLoaded", ()=> {
         },
         scrollTrigger : {
             trigger : dropDownTrigger,
-            markers : true,
+            // markers : true,
             start : "top 25%"
         }
 
     })
 
+    // FEATURED VEHICLES GSAP EFFECTS
+    const f1 = document.querySelector('.f1')
+    const f2 = document.querySelector('.f2')
+    const f3 = document.querySelector('.f3')
+    const f_trigger = document.querySelector(".Featured-Vehicle-trigger")
+    const f_trigger_1 = document.querySelector(".Featured-Vehicle-trigger-f1")
+    const f_trigger_2 = document.querySelector(".Featured-Vehicle-trigger-f2")
+    const f_trigger_3 = document.querySelector(".Featured-Vehicle-trigger-f3")
+
+    const f_One_timeLine = gsap.timeline()
+
+    f_One_timeLine.from(f1,{
+        duration : 2,
+        opacity : 0,
+        x : -100,
+        ease : "power2.out",
+        scrollTrigger : {
+            trigger : f_trigger,
+            start : "top center"
+        }
+    })
+
+    f_One_timeLine.from(f2,{
+        duration : 2,
+        opacity : 0,
+        scale : 0.5,
+        y : 150, 
+        ease :"power2.out",scrollTrigger : {
+            trigger : f_trigger,
+            start : "top center"
+        }
+    })
+    f_One_timeLine.from(f3,{
+        duration : 2,
+        opacity : 0,
+        x : 100, 
+        ease : "power2.out",scrollTrigger : {
+            trigger : f_trigger,
+            start : "top center"
+        }
+    })
 
  
 })
@@ -76,27 +117,4 @@ document.addEventListener("DOMContentLoaded", ()=> {
 
 
 
-       //FEATURED VEHICLES GSAP EFFECTS
-    // const f1 = document.querySelector('.f1')
-    // const f2 = document.querySelector('.f2')
-    // const f3 = document.querySelector('.f3')
-
-    // const f_One_timeLine = gsap.timeline()
-
-    // f_One_timeLine.from(f1,{
-    //     duration : 2,
-    //     opacity : 0,
-    //     x : -100
-    // })
-
-    // f_One_timeLine.from(f2,{
-    //     duration : 2,
-    //     opacity : 0,
-    //     scale : 0.5,
-    //     y : 50
-    // })
-    // f_One_timeLine.from(f3,{
-    //     duration : 2,
-    //     opacity : 0,
-    //     x : 100
-    // })
+    
