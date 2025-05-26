45c0baf90e6101b76ddd0f222716d0e2a07ec40d
TimiMaliki <malikitimi@gmail.com>
2025-05-26 11:08:18 +0100

    gsap

--- InterDecor/src/js/main.js
+++ InterDecor/src/js/main.js
@@ -1,56 +1,81 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  gsap.registerPlugin(ScrollToPlugin);
 
- document.addEventListener("DOMContentLoaded", (event) => {
-  gsap.registerPlugin(ScrollToPlugin)
+  //   Brand name Animation
+  const brand = document.querySelector(".brand");
 
-
-//   Brand name Animation
- const brand = document.querySelector('.brand')
-
-gsap.fromTo(brand,{
-      y : -270,
-      scale : 5,
-}, {
-    y : 0, 
-    duration : 1,
-    scale : 1,
-    color : "#fff",
-    scrollTrigger : {
-        trigger : brand,
-        end : "bottom top",
-        invalidateOrRefresh : true,
-        scrub : 2,
-        ease : "expoScale(0.5,7,none)",
-       
+  gsap.fromTo(
+    brand,
+    {
+      y: -270,
+      scale: 5,
+    },
+    {
+      y: 0,
+      duration: 1,
+      scale: 1,
+      color: "#fff",
+      scrollTrigger: {
+        trigger: brand,
+        end: "bottom top",
+        invalidateOrRefresh: true,
+        scrub: 2,
+        ease: "expoScale(0.5,7,none)",
+      },
     }
-})
+  );
 
-//  slide container
- const slide = document.querySelector(".slide")
- const slide1 = document.querySelector(".slide-1")
- const slide2 = document.querySelector(".slide-2")
- const slide3 = document.querySelector(".slide-3")
- const slide4 = document.querySelector(".slide-4")
+  //  slide container
+  const slide = document.querySelector(".slide");
+  const slide1 = document.querySelector(".slide-1");
+  const slide2 = document.querySelector(".slide-2");
+  const slide3 = document.querySelector(".slide-3");
+  const slide4 = document.querySelector(".slide-4");
 
- const SlideTl = gsap.timeline({
-    scrollTrigger : {
-        trigger : slide,
-         // markers: true,
-        start :"top 20%",
-        scrub : 2,
-        ease : "power2.out"
-    }})
+  const SlideTl = gsap.timeline({
+    scrollTrigger: {
+      trigger: slide,
+      // markers: true,
+      start: "top 20%",
+      scrub: 2,
+      ease: "power2.out",
+    },
+  });
 
-    SlideTl.to(slide1,{
-        y : 220,
-    })
-    SlideTl.to(slide2,{
-        y : 220,
-    })
-    SlideTl.to(slide3,{
-        y : 220,
-    })
-    SlideTl.to(slide4,{
-        y : 220,
-    })
+  SlideTl.to(slide1, {
+    y: 220,
+  });
+  SlideTl.to(slide2, {
+    y: 220,
+  });
+  SlideTl.to(slide3, {
+    y: 220,
+  });
+  SlideTl.to(slide4, {
+    y: 220,
+  });
 
- });
+  // images section container
+
+  const imageSecOne = document.querySelector(".imageSecOne");
+  const imageSecOneContainer = document.querySelector(".img-container");
+
+  gsap.to(imageSecOne, {
+    clipPath: "circle(1000% at 50% 50%)",
+    scrollTrigger: {
+      trigger: imageSecOneContainer,
+      markers: true,
+      start: "top top",
+      end : "bottom bottom",
+      scrub: 2,
+      pin : true ,
+      onEnter : () => {
+        document.body.classList.add("lightTheme")
+      },
+      onLeaveBack: () => {
+        document.body.classList.remove("lightTheme")
+      },
+      ease: "power2.out",
+    },
+  });
+});
