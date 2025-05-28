66b914de6a9edc88e09c95340a424ba8472fedfd
TimiMaliki <malikitimi@gmail.com>
2025-05-28 22:01:37 +0100

    footer

---
+++ Studio-i.O/src/js/index.js
@@ -0,0 +1,12 @@
+const hamburgerMenu = document.querySelector(".menuBar")
+const navLinks = document.querySelector(".nav-links")
+
+
+// -------------- HAMBURGERMENU --------------
+
+hamburgerMenu.addEventListener("click" , ()=>{
+  navLinks.classList.toggle("active")
+})
+
+// -------------- ANIMATION --------------
+
