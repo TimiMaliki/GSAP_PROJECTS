46981528518a7af655cc2d22192e74de5e38690c
TimiMaliki <malikitimi@gmail.com>
2025-05-29 04:10:05 +0100

    gsap-animation

--- Studio-i.O/src/js/index.js
+++ Studio-i.O/src/js/index.js
@@ -10,7 +10,7 @@ hamburgerMenu.addEventListener("click" , ()=>{
 
 // -------------- ANIMATION --------------
 
-const els = ['#hero-container  .animate span', "#work-section .animate span" , "#brand .animate span"]
+const els = ['#hero-container  .animate span', '.work-section .animate span' , "#brand .animate span" , "#footer .animate span"]
 
 els.forEach((el)=>{
         gsap.from(el , {
@@ -29,4 +29,45 @@ els.forEach((el)=>{
 
         })
 })
+
+
+const imageAnimation = document.querySelector("#image-container .over-lay")
+
+gsap.to(imageAnimation , {
+    scrollTrigger : {
+        trigger : "#image-container",
+        start : "top 90%",
+        end : "bottom bottom",
+        scrub : 1,
+    },
+
+    width : "86vw"
+
+})
+
+
+const recentProject = document.querySelectorAll(".items")
+const projectTrigger = document.querySelector("#recent-project-container  .grid .items")
+
+recentProject.forEach((project)=>{
+    gsap.fromTo(project,{
+        opacity : 0,
+        y : 100
+    }, {
+        opacity : 1,
+        y : 0,
+        duration : 1,
+        stagger : 0.2,
+        scrollTrigger : {
+            trigger : projectTrigger,
+            start : "top 70%",
+            end : "bottom 70%",
+            toggleActions : "play none none none",
+        }
+    })
+})
+
+
+
+
 
