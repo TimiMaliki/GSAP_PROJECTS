fbab71d0129ff1d6d8e97349a189df2dc8cc85c3
TimiMaliki <malikitimi@gmail.com>
2025-05-29 03:29:01 +0100

    gsap-animation

--- Studio-i.O/src/js/index.js
+++ Studio-i.O/src/js/index.js
@@ -10,3 +10,23 @@ hamburgerMenu.addEventListener("click" , ()=>{
 
 // -------------- ANIMATION --------------
 
+const els = ['#hero-container  .animate span', "#work-section .animate span" , "#brand .animate span"]
+
+els.forEach((el)=>{
+        gsap.from(el , {
+            y : 300,
+            rotation : 15,
+            duration : 4,
+            ease : 'power3.out',
+
+            scrollTrigger : {
+                trigger : el,
+                start : "top 120%",
+                end : "bottom 90%",
+                toggleActions : "play none none none",
+            }
+
+
+        })
+})
+
